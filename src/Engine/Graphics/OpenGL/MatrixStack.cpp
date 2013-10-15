cdf7c8bf7b2d7b13d6b5675e45cd0ce1ac21cd9b
Ben <hhyyxx008@hotmail.com>
2013-10-15 20:47:34 +0800

    (src) : progress
    - completed MatrixStack.

--- src/Engine/Graphics/OpenGL/MatrixStack.cpp
+++ src/Engine/Graphics/OpenGL/MatrixStack.cpp
@@ -2,25 +2,25 @@
 
 namespace KG
 {
-	const glm::dmat4 MatrixStack::sk_IdentityMatrix;
 
 	MatrixStack::MatrixStack(void)
-		: m_PushEngaged(false), m_TopMatrix(1.0f)
+		: m_TopMatrix(1.0f)
+	{}
+
+	void MatrixStack::ApplyMatrix(const glm::dmat4 & p_rMat)
 	{
+		m_TopMatrix = p_rMat * m_TopMatrix;
 	}
 
 	void MatrixStack::Push(void)
 	{
-		m_PushEngaged = true;
 		m_MatrixStack.push(m_TopMatrix);
-		m_TopMatrix = glm::dmat4(1.0f);
-
+		m_TopMatrix = std::move(glm::dmat4(1.0));
 	}
 
 	void MatrixStack::Pop(void)
 	{
 		if (m_MatrixStack.empty()) KE::Debug::print(KE::Debug::DBG_ERROR, "MatrixStack : stack empty.");
-		m_PushEngaged = false;
 		m_TopMatrix = m_MatrixStack.top();
 		m_MatrixStack.pop();
 	}
@@ -37,8 +37,9 @@ namespace KG
 
 	void MatrixStack::SetIdentity(void)
 	{
-		m_TopMatrix = glm::dmat4(1.0);
+		m_TopMatrix = std::move(glm::dmat4());
 	}
+
 
 	PushMatrixStack::PushMatrixStack(KG::MatrixStack & p_rMatrixStack)
 		: m_rMatrixStack(p_rMatrixStack)
--- src/Engine/Graphics/OpenGL/MatrixStack.hpp
+++ src/Engine/Graphics/OpenGL/MatrixStack.hpp
@@ -10,23 +10,24 @@ namespace KG
 	class MatrixStack
 	{
 	private:
-		static const glm::dmat4		sk_IdentityMatrix;
-		std::stack<glm::dmat4>	m_MatrixStack;
-		bool					m_PushEngaged;
-		glm::dmat4				m_TopMatrix;
+		std::stack<glm::dmat4>		m_MatrixStack;
+		glm::dmat4					m_TopMatrix;
 
 	public:
 		MatrixStack(void);
 
-		/*! Save the current top matrix into the stack. */
+		/*! Right multiple the current matrix with the given one. Result matrix becomes the current one. */
+		void ApplyMatrix(const glm::dmat4 & p_rMat);
+		/*! Save/perserve the current top matrix into the stack. */
 		void Push(void);
-
+		/*! Remove the top matrix. Unicorn will poop atomic bombs if there isn't matrix already on the stack. */
 		void Pop(void);
+		/*! Get const reference to top matrix. */
 		const glm::dmat4 & Top(void) const;
 
-		/*! Set given matrix as top matrix. */
+		/*! Set the current/top matrix to the given matrix. */
 		void SetMatrix(const glm::dmat4 & p_Mat4);
-		/*! Set top matrix to identity matrix. */
+		/*! Set current/top matrix to identity matrix. */
 		void SetIdentity(void);
 	};
 
